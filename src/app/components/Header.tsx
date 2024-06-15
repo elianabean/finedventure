01c93aa45dd7274569727aea8f018aa75b4284f4
elianabean <ewanginspiration@gmail.com>
2024-06-14 22:51:53 -0400

    header ui fixes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,36 +1,80 @@
-import {
-    Navbar,
-    NavbarBrand,
-    NavbarContent,
-    NavbarItem,
-    Button,
-    Link,
-  } from "@nextui-org/react";
-  import React from "react";
+'use client'
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
+import React from "react";
   
-  export default function Header() {
-    return (
-      <Navbar className="bg-transparent fixed h-[10vh]" maxWidth="full" isBlurred={true}>
-        <NavbarBrand className="px-[80px]">
-            <p className="text-primary font-raleway text-[55px] italic font-bold leading-normal">FinEdVenture</p>
-        </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-[53px] mr-[260px]" justify="start">
-            <NavbarItem>
-                    <div className="flex flex-row shrink-0 justify-center align-center rounded-[20px] border-[1px] border-solid bg-[#ECFFF6]  py-[9px] px-[20px]">
-                        <p className="text-primary font-lato text-3xl font-medium leading-normal ">Our Team</p>
-                    </div>
-            </NavbarItem>
-            <NavbarItem>
-                    <div className="flex flex-row shrink-0 justify-center align-center rounded-[20px] border-[1px] border-solid bg-[#ECFFF6]  py-[9px] px-[20px]">
-                        <p className="text-primary font-lato text-3xl font-medium leading-normal ">Financial Literacy</p>
-                    </div>
-            </NavbarItem>
-            <NavbarItem>
-                    <div className="flex flex-row shrink-0 justify-center align-center rounded-[20px] border-[1px] border-solid bg-[#ECFFF6]  py-[9px] px-[20px]">
-                        <p className="text-primary font-lato text-3xl font-medium leading-normal ">Business Competitons</p>
-                    </div>
-            </NavbarItem>
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  return (
+    <Navbar className="bg-transparent fixed h-[10vh]" maxWidth="full" isBlurred={true} onMenuOpenChange={setIsMenuOpen}>
+      <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+      <NavbarBrand className="px-[80px]">
+          <p className="text-primary font-raleway text-[55px] italic font-bold leading-normal">FinEdVenture</p>
+      </NavbarBrand>
+
+      <NavbarContent className="hidden sm:flex gap-[53px]" justify="center">
+          <NavbarItem>
+            <Link href="#">
+              <p className="text-primary font-lato text-2xl font-medium leading-normal ">Our Team</p>
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href="#">
+              <p className="text-primary font-lato text-2xl font-medium leading-normal ">Financial Literacy</p>
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href="#">
+              <p className="text-primary font-lato text-2xl font-medium leading-normal ">Business Competitons</p>
+            </Link>
+          </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+      <NavbarItem>
+            <Button as={Link} className="border-primary border-1 text-2xl p-6 bg-transparent text-primary" href="#" variant="flat">
+              Login
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            <Button as={Link} color="primary" className=" text-2xl p-6" href="#" variant="flat">
+              Sign Up
+            </Button>
+          </NavbarItem>
         </NavbarContent>
-      </Navbar>
-    );
-  }
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
